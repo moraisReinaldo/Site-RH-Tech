22c3d148a907316886a35369d136772d8ae68cf0
Reinaldo Henrique Morais <116378866+RHMestudante@users.noreply.github.com>
2022-10-21 15:09:55 -0300

    FormLogin js começo

---
+++ Paginas/Formularios/FormLogin.js
@@ -0,0 +1,14 @@
+function login(){
+
+    let nome = document.getElementById("userName").value;
+
+    let senha = document.getElementById("pwd").value;
+
+    let cadastro = document.getElementById("texto")
+    cadastro.innerHTML = ("Cadastro concluido!");
+
+    window.location.href = "..//PaginaIncial//PaginaInicial.html";
+
+    alert("OIasdassad")
+
+}
