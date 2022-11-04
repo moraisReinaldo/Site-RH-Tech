df1caf9698ace54dd1b9dbaab871e9cbf8513d18
Reinaldo Henrique Morais <116378866+RHMestudante@users.noreply.github.com>
2022-11-04 14:58:10 -0300

    Foto que troca com js

--- Paginas/PaginaIncial/Paginainicial.js
+++ Paginas/PaginaIncial/Paginainicial.js
@@ -1,3 +1,12 @@
-function trocaImagem(){
+function trocaImagem(objeto, caminhoNovaImagem, caminhoAntigo){
 
+    let img = document.getElementById(objeto)
+
+    if(img.src == caminhoNovaImagem){
+        
+        document.getElementById(objeto).src = caminhoAntigo;
+    }else{
+
+        document.getElementById(objeto).src = caminhoNovaImagem; 
+    }
 }
